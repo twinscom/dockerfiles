7dad49632308ebbd1dbf5b07198bb7a551f8c76e
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2018-08-20 15:33:29 +0300

    Fix backupper

--- backupper/postgres/run.sh
+++ backupper/postgres/run.sh
@@ -5,6 +5,9 @@ set -o nounset
 
 IFS=$(printf '\n\t')
 
+chmod 644 /etc/logrotate.d/*
+chown root:root /etc/logrotate.d/*
+
 touch "/backups/${BACKUP_NAME:-postgres-backup.sql}"
 
 exec crond -f
--- backupper/rethinkdb/run.sh
+++ backupper/rethinkdb/run.sh
@@ -5,6 +5,9 @@ set -o nounset
 
 IFS=$(printf '\n\t')
 
+chmod 644 /etc/logrotate.d/*
+chown root:root /etc/logrotate.d/*
+
 touch "/backups/${BACKUP_NAME:-rethinkdb-backup.tar.gz}"
 
 exec cron -f
