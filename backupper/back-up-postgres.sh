5f7cc5ecd7e08b6f12f0297059e28a5c36e8fd2a
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2018-08-23 14:40:25 +0300

    Fix backupper/back-up-postgres.sh

--- backupper/back-up-postgres.sh
+++ backupper/back-up-postgres.sh
@@ -6,11 +6,16 @@ set -o nounset
 IFS=$(printf '\n\t')
 
 PGHOST=${PGHOST:-postgres}
+PGPORT=${PGPORT:-5432}
 PGUSER=${PGUSER:-postgres}
 
 BACKUP_FILE="/backups/$(date +'%Y-%m-%dT%H:%M:%S').sql"
 
-pg_dumpall "--file=$BACKUP_FILE"
+pg_dumpall \
+    "--host=$PGHOST" \
+    "--port=$PGPORT" \
+    "--username=$PGUSER" \
+    "--file=$BACKUP_FILE"
 
 gzip -9 "$BACKUP_FILE"
 
